99c8acfb5b62140158fd0430318e6afa7e5ff73e
abot8639 <abot8639@gmail.com>
2026-06-23 03:09:49 +0300

    1

---
+++ bin/particle_blob.dart
@@ -0,0 +1,5 @@
+import 'package:particle_blob/particle_blob.dart' as particle_blob;
+
+void main(List<String> arguments) {
+  print('Hello world: ${particle_blob.calculate()}!');
+}
---
+++ test/particle_blob_test.dart
@@ -0,0 +1,8 @@
+import 'package:particle_blob/particle_blob.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
